a49a74e7dab1b97073d9a46df3748742d3957d18
51bitquant <stephen0823@aliyun.com>
2022-06-05 15:54:32 +0800

    add video-instruction.md

---
+++ Video-Instruction.md
@@ -0,0 +1,194 @@
+# Video tutorial on how to setup the binance_grid_trader
+
+## softwares requirements.
+1. install pycharm, go to the website and download it: https://www.jetbrains.com/pycharm/download/
+2. install anaconda, url: https://www.anaconda.com/products/distribution
+
+remember to click "add conda to your path", we need to use the conda in
+terminal. If you encounter conda not found, check out this link: https://stackoverflow.com/questions/35246386/conda-command-not-found
+
+
+## download the codes
+
+download the codes from github:
+https://github.com/51bitquant/binance_grid_trader.git 
+
+## create python environment and configure project's interpreter
+
+use the anaconda to create a virtual environment, here we create a
+virtual env name trader, you can use any name you like:
+
+> conda create -n trader python==3.7
+
+then you have an env(trader), to use this env, you can use the following
+command to activate the env.
+
+> conda activate trader
+
+Second, you need to use the pip to install the libraries.
+
+> pip install -r requirements.txt
+
+the requirements.txt is in the project. It lists the libraries the
+project rely on.
+
+After install the libraries, you can run the main.py, if you use the
+pycharm, you may need to configure your project's interpreter.
+
+> python main.py
+
+## how to use
+
+### connect Binance future api
+
+<img src="resources/connect_future_usdt.png" alt="connect_future_usdt"/>
+
+
+1. key: past your api key from binance exchange Api management here.
+
+2. secret: past your api secret from binance exchange Api management, and remember to edit your api restrictions to allow trade futures. 
+
+3. futures_types: if you want to trade contracts settled in USDT or BUSD like BTCUSDT, BTCBUSD symbol, set it to USDT else set it to COIN.
+
+4. proxy_host and proxy_port, if you can connect to binance exchange, then leave it blank. If you can't connect directly to Binance exchange, you may need to config the proxy_host or proxy_port.
+If you have run vpn software, then set the proxy_host to 127.0.0.1 else you need to set the proxy_host to a remote server ip.
+
+All your config api data will save in project directory
+gridtrader/connect_futures.json file.
+
+NOTE: Remember every time you run the grid strategy, you need to connect
+the api your api First.To connect the binance api, Click the confirm
+button.
+
+## connect Binance Spot Api
+Click the top-left Config Binance Api button, near the python button and
+select Connect Spot.
+
+<img src="resources/connect_spot.png" alt="connect_future_usdt"/>
+
+1. key: past your api key from binance exchange Api management here.
+
+2. secret: past your api secret from binance exchange Api management, and remember to edit your api restrictions to allow trade futures. 
+
+3. proxy_host and proxy_port, if you can connect to binance exchange,
+  then leave it blank. If you can't connect directly to Binance
+  exchange, you may need to config the proxy_host or proxy_port. If you
+  have run vpn software, then set the proxy_host to 127.0.0.1 else you
+  need to set the proxy_host to a remote server ip.
+
+All your config api data will save in project directory
+gridtrader/connect_spot.json file.
+
+NOTE: Remember every time you run the grid strategy, you need to connect
+the api your api First.To connect the binance api, Click the confirm
+button.
+
+## Add Futures Grid Strategy
+
+<img src="resources/add_future_grid_strategy.png" alt="window picture"/>
+If you want to run the grid strategy in Futures Market, you can add the FutureGridStrategy. The following is the parameters description for the strategy:
+
+1. strategy_name: set a name for the strategy.
+2. vt_symbol: the symbol you want to trade, like BTCUSDT, ETHBUSD ect.
+   for Future, remember to use the uppercase. if you use the lower case,
+   you will run in the Spot market.
+   
+3. upper_price: the grid highest price, if you set the upper_price to
+   40000, that means if the market price is over 40000, the strategy
+   won't place any order.
+   
+4. bottom_price: the lowest price fo the strategy. If you set the
+   bottom_price to 30000, if the price is under 30000, the strategy
+   won't place any order.
+
+5. grid_number, the grid number for the strategy, if the upper_price is
+   40000, the bottom_price is 30000, if the grid_number is 100, the grid
+   step(or the order price step) is (40000- 30000)/100 = 100
+   
+6. order_volume: your order volume/quantity
+
+7. max_open_orders: the max open order in buy/sell side.
+
+The strategy settings data will save in
+gridtrader/grid_strategy_setting.json, you can checkout and modify it.
+
+And the gridtrader/grid_strategy_data.json file will save some variables
+in the strategy when runnning.
+
+## Add Spot Grid Strategy 
+
+<img src="resources/add_spot_grid_strategy.png" alt="window picture"/>
+
+If you want to run the grid strategy in Spot Market, you can add the
+SpotGridStrategy. The following is the parameters description for the
+strategy:
+
+1. strategy_name: set a name for the strategy.
+2. vt_symbol: the symbol you want to trade, like btcusdt, ethbusd ect.
+   for spot market, remember to use the lowercase. if you use the
+   lower case, you will run in the Spot market.
+   
+3. upper_price: the grid highest price, if you set the upper_price to
+   40000, that means if the market price is over 40000, the strategy
+   won't place any order.
+   
+4. bottom_price: the lowest price fo the strategy. If you set the
+   bottom_price to 30000, if the price is under 30000, the strategy
+   won't place any order.
+
+5. grid_number, the grid number for the strategy, if the upper_price is
+   40000, the bottom_price is 30000, if the grid_number is 100, the grid
+   step(or the order price step) is (40000- 30000)/100 = 100
+   
+6. order_volume: your order volume/quantity
+
+7. invest_coin: if you want to trade btcusdt, then set to USDT, if you
+   want to trade btcbusd, then set to BUSD, if you want to trade ethbtc,
+   then set to btc.
+
+8. max_open_orders: the max open order in buy/sell side.
+
+The strategy settings data will save in
+gridtrader/grid_strategy_setting.json, you can checkout and modify it.
+
+And the gridtrader/grid_strategy_data.json file will save some variables
+in the strategy when runnning.
+
+
+## Start Your Grid Strategy
+
+<img src="resources/start_strategy.png" alt="window picture"/>
+
+If you want to start the grid strategy, you need to:
+1. Init your strategy: Click the Init Button
+
+2. Start your strategy: Click the Start Button.
+
+If you want to stop the strategy, click the Stop button, or even click
+edit button to edit the strategy's parameters.
+
+If you have a lot of strategies to start in batch, you can click Init
+All Strategies and Start All Strategies.
+
+
+## using script
+If want to run the code in Linux system or just don't want to use the
+UI, you can use the script. If you want to trade in spot market,
+checkout the main_spot_script.py. If you want to trade in the future
+market, checkout the main_futures_script.py
+
+But before run the script, you need to edit the
+gridtrader/grid_strategy_setting.json file, then config your strategy
+setting.
+
+
+## contact
+wechat: bitquant51 
+
+discord: 51bitquant#8078
+
+IF you have any question, please feel free to contact me in discord. If
+you don't have a binance account, I'm happy you can use the following
+link to register the account:
+[Binance referral LINK](https://accounts.binance.com/zh-CN/register?ref=ESE80ESH),
+and you will get 20% commission rebate.
---
+++ gridtrader/trader/strategies/future_grid_long_short_strategy.py
@@ -0,0 +1,233 @@
+from ..engine import CtaEngine, EVENT_TIMER
+
+from gridtrader.trader.object import Status
+from typing import Union, Optional
+from gridtrader.trader.utility import floor_to
+from gridtrader.trader.object import OrderData, TickData, TradeData, ContractData
+from .template import CtaTemplate
+from gridtrader.trader.utility import GridPositionCalculator
+
+
+class FutureGridStrategy(CtaTemplate):
+    """
+    币安合约中性网格
+    策略在震荡行情下表现很好，但是如果发生趋势行情，单次止损会比较大，导致亏损过多。
+
+    免责声明: 本策略仅供测试参考，本人不负有任何责任。使用前请熟悉代码。测试其中的bugs, 请清楚里面的功能后再使用。
+    币安邀请链接: https://www.binancezh.pro/cn/futures/ref/51bitquant
+    合约邀请码：51bitquant
+
+
+    Disclaimer:
+    Invest in Crypto currency is high risk. Take care of yourself. I am not responsible for your investment.
+    Binance Referral Link: https://www.binancezh.pro/cn/futures/ref/51bitquant
+
+    """
+    author = "51bitquant"
+
+    # parameters
+    upper_price = 0.0  # The grid strategy high/upper price 执行策略的最高价.
+    bottom_price = 0.0  # The grid strategy low/bottom price 执行策略的最低价.
+    grid_number = 100  # grid number 网格的数量.
+    order_volume = 0.05  # order volume  每次下单的数量.
+    max_open_orders = 5  # max open price  一边订单的数量.
+
+    # the strategy will stop when the price break the upper/bottom price, if you set the close_position when stop True,
+    # it will automatically close your position.
+
+    # variables
+    avg_price = 0.0  # current average price for the position  持仓的均价
+    step_price = 0.0  # price step between two grid 网格的间隔
+    trade_times = 0  # trade times
+
+    parameters = ["upper_price", "bottom_price", "grid_number", "order_volume", "max_open_orders"]
+
+    variables = ["avg_price", "step_price", "trade_times"]
+
+    def __init__(self, cta_engine: CtaEngine, strategy_name, vt_symbol, setting):
+        """"""
+        super().__init__(cta_engine, strategy_name, vt_symbol, setting)
+
+        self.long_orders_dict = {}  # long orders dict {'orderid': price}
+        self.short_orders_dict = {}  # short orders dict {'orderid': price}
+
+        self.tick: Union[TickData, None] = None
+        self.contract_data: Optional[ContractData] = None
+
+        self.pos_calculator = GridPositionCalculator()
+        self.timer_count = 0
+
+    def on_init(self):
+        """
+        Callback when strategy is inited.
+        """
+        self.write_log("Init Strategy")
+
+    def on_start(self):
+        """
+        Callback when strategy is started.
+        """
+        self.write_log("Start Strategy")
+        self.contract_data = self.cta_engine.main_engine.get_contract(self.vt_symbol)
+
+        if not self.contract_data:
+            self.write_log(f"Could Not Find The Symbol:{self.vt_symbol}, Please Connect the Api First.")
+            self.inited = False
+        else:
+            self.inited = True
+
+        self.pos_calculator.pos = self.pos
+        self.pos_calculator.avg_price = self.avg_price
+
+        self.cta_engine.event_engine.register(EVENT_TIMER, self.process_timer)
+
+    def on_stop(self):
+        """
+        Callback when strategy is stopped.
+        """
+        self.write_log("Stop Strategy")
+        self.cta_engine.event_engine.unregister(EVENT_TIMER, self.process_timer)
+
+    def process_timer(self, event):
+        self.timer_count += 1
+        if self.timer_count >= 10:
+            self.timer_count = 0
+
+            # remove the order(highest price order for short, lowest price to for long)
+            # to keep the max open order meet requirements
+            if len(self.long_orders_dict.keys()) > self.max_open_orders:
+
+                vt = list(self.long_orders_dict.keys())[0]
+                lowest_price = self.long_orders_dict[vt]
+                cancel_order_id = None
+
+                for orderid in self.long_orders_dict.keys():
+                    order_price = self.long_orders_dict[orderid]
+                    if lowest_price >= order_price:
+                        cancel_order_id = orderid
+                        lowest_price = order_price
+
+                if cancel_order_id:
+                    self.cancel_order(cancel_order_id)
+
+            if len(self.short_orders_dict.keys()) > self.max_open_orders:
+
+                vt = list(self.short_orders_dict.keys())[0]
+                highest_price = self.short_orders_dict[vt]
+                cancel_order_id = None
+
+                for orderid in self.short_orders_dict.keys():
+                    order_price = self.short_orders_dict[orderid]
+                    if highest_price <= order_price:
+                        cancel_order_id = orderid
+                        highest_price = order_price
+
+                if cancel_order_id:
+                    self.cancel_order(cancel_order_id)
+
+            self.put_event()
+
+    def on_tick(self, tick: TickData):
+        """
+        Callback of new tick data update.
+        """
+
+        if tick and tick.bid_price_1 > 0 and self.contract_data:
+            self.tick = tick
+
+            if self.upper_price - self.bottom_price <= 0:
+                return
+
+            step_price = (self.upper_price - self.bottom_price) / self.grid_number
+
+            self.step_price = float(floor_to(step_price, self.contract_data.price_tick))
+
+            mid_count = round((float(self.tick.bid_price_1) - self.bottom_price) / self.step_price)
+
+            if len(self.long_orders_dict.keys()) == 0:
+
+                for i in range(self.max_open_orders):
+                    price = self.bottom_price + (mid_count - i - 1) * self.step_price
+                    if price < self.bottom_price:
+                        return
+
+                    orders_ids = self.buy(price, self.order_volume)
+                    for orderid in orders_ids:
+                        self.long_orders_dict[orderid] = price
+
+            if len(self.short_orders_dict.keys()) == 0:
+                for i in range(self.max_open_orders):
+                    price = self.bottom_price + (mid_count + i + 1) * self.step_price
+                    if price > self.upper_price:
+                        return
+
+                    orders_ids = self.short(price, self.order_volume)
+
+                    for orderid in orders_ids:
+                        self.short_orders_dict[orderid] = price
+
+    def on_order(self, order: OrderData):
+        """
+        Callback of new order data update.
+        """
+        if order.vt_orderid not in (list(self.short_orders_dict.keys()) + list(self.long_orders_dict.keys())):
+            return
+
+        self.pos_calculator.update_position(order)
+        self.avg_price = self.pos_calculator.avg_price
+
+        if order.status == Status.ALLTRADED:
+
+            if order.vt_orderid in self.long_orders_dict.keys():
+                del self.long_orders_dict[order.vt_orderid]
+
+                self.trade_times += 1
+
+                short_price = float(order.price) + float(self.step_price)
+
+                if short_price <= self.upper_price:
+                    orders_ids = self.short(short_price, self.order_volume)
+
+                    for orderid in orders_ids:
+                        self.short_orders_dict[orderid] = short_price
+
+                if len(self.long_orders_dict.keys()) < self.max_open_orders:
+                    long_price = float(order.price) - float(self.step_price) * self.max_open_orders
+                    if long_price >= self.bottom_price:
+                        orders_ids = self.buy(long_price, self.order_volume)
+                        for orderid in orders_ids:
+                            self.long_orders_dict[orderid] = long_price
+
+            if order.vt_orderid in self.short_orders_dict.keys():
+                del self.short_orders_dict[order.vt_orderid]
+
+                self.trade_times += 1
+                long_price = float(order.price) - float(self.step_price)
+                if long_price >= self.bottom_price:
+                    orders_ids = self.buy(long_price, self.order_volume)
+                    for orderid in orders_ids:
+                        self.long_orders_dict[orderid] = long_price
+
+                if len(self.short_orders_dict.keys()) < self.max_open_orders:
+                    short_price = float(order.price) + float(self.step_price) * self.max_open_orders
+
+                    if short_price <= self.upper_price:
+                        orders_ids = self.short(short_price, self.order_volume)
+                        for orderid in orders_ids:
+                            self.short_orders_dict[orderid] = short_price
+
+        if not order.is_active():
+            if order.vt_orderid in self.long_orders_dict.keys():
+                del self.long_orders_dict[order.vt_orderid]
+
+            elif order.vt_orderid in self.short_orders_dict.keys():
+
+                del self.short_orders_dict[order.vt_orderid]
+
+        self.put_event()
+
+    def on_trade(self, trade: TradeData):
+        """
+        Callback of new trade data update.
+        """
+        self.put_event()
